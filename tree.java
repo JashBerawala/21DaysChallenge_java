a8a30318879c0e600dd732adec14e7983f37816f
JashBerawala <62662365+JashBerawala@users.noreply.github.com>
2020-03-26 22:30:23 +0530

    Create tree.java

---
+++ tree.java
@@ -0,0 +1,17 @@
+public class Tree
+{
+  public static void main(String[]  args)
+  {
+    System.out.println("Hey!!This is Jash Berawala ");
+    System.out.println("I wil be printing a treee ");
+   System.out.println("   ********     ");
+   System.out.println("  **********    ");
+   System.out.println(" *************  ");
+   System.out.println("****************");
+   System.out.println("   ***********  ");
+   System.out.println("     ******       ");
+   System.out.println("     ******       ");
+   System.out.println("     ******       ");
+     
+    }
+}
